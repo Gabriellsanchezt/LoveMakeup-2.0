fde0866a1c05c1ff4b70291bd7ac85ddc524bec7
Miruto80 <miguelfernando8012@gmail.com>
2026-04-18 18:13:55 -0400

    Cambiando variables

--- config/config.php
+++ config/config.php
@@ -1,9 +1,11 @@
 <?php
  
-define('DB_NAME_1', 'lovemakeupbd1');
-define('DB_NAME_2', 'lovemakeupbds2');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', ''); 
+define('_DB_NAME_1_', 'u417714339_Lovemakeupbd');
+define('_DB_NAME_2_', 'u417714339_lovemakeupbds');
+define('_DB_HOST_', 'localhost');
+define('_DB_USER_', 'u417714339_Lovemakeup');
+define('_DB_USER_2_', 'u417714339_lovemakeups');
+define('_DB_PASS_', 'Labiosfresa123.'); 
 
 date_default_timezone_set('America/Caracas');
+?>
